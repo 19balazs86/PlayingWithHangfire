3be3e74133548e61f52c40ce64231bb068c314a8
Balázs Keresztúri <19balazs86@gmail.com>
2019-11-17 20:41:30 +0000

    Add basic setup for Hangfire.

--- PlayingWithHangfire/Startup.cs
+++ PlayingWithHangfire/Startup.cs
@@ -1,3 +1,6 @@
+using System;
+using Hangfire;
+using Hangfire.SqlServer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,14 +21,42 @@ namespace PlayingWithHangfire
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+
+      string connString = _configuration.GetConnectionString("HangfireConnection");
+
+      services.AddHangfire(configuration =>
+      {
+        var serverStorageOptions = new SqlServerStorageOptions
+        {
+          SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+          QueuePollInterval          = TimeSpan.Zero,
+          PrepareSchemaIfNecessary   = true
+        };
+
+        configuration.UseSqlServerStorage(connString, serverStorageOptions);
+      });
+
+      services.AddHangfireServer(options =>
+      {
+        options.WorkerCount             = 5;
+        options.SchedulePollingInterval = TimeSpan.FromSeconds(10);
+      });
     }
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(
+      IApplicationBuilder app,
+      IWebHostEnvironment env,
+      IBackgroundJobClient backgroundJobs)
     {
       if (env.IsDevelopment())
         app.UseDeveloperExceptionPage();
 
       app.UseRouting();
+
+      // http://localhost:5000/hangfire
+      app.UseHangfireDashboard();
+
+      backgroundJobs.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
 
       app.UseAuthorization();
 
